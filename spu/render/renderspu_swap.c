1b826b0bafc503b46526752cd1e7333572a0530d
Greg Humphreys <humper@users.sourceforge.net>
2001-02-10 00:40:51 +0000

    Removed extraneous debugging error printouts that have been subsumed by crError

--- spu/render/renderspu_swap.c
+++ spu/render/renderspu_swap.c
@@ -22,28 +22,6 @@ void SPU_APIENTRY renderspuSwapBuffers( void )
 	//render_spu.dispatch->DrawBuffer( GL_FRONT );
 	if (!render_spu.wglSwapBuffers( render_spu.device_context ))
 	{
-		static char buf[512], *temp;
-		DWORD err = GetLastError();
-
-		sprintf( buf, "err=%d", err );
-
-		FormatMessage( FORMAT_MESSAGE_ALLOCATE_BUFFER |
-				FORMAT_MESSAGE_FROM_SYSTEM |
-				FORMAT_MESSAGE_MAX_WIDTH_MASK, NULL, err,
-				MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
-				(LPTSTR) &temp, 0, NULL );
-		if ( temp )
-		{
-			crStrncpy( buf, temp, sizeof(buf)-1 );
-		}
-
-		temp = buf + crStrlen(buf) - 1;
-		while ( temp > buf && isspace( *temp ) )
-		{
-			*temp = '\0';
-			temp--;
-		}
-
 		crError( "wglSwapBuffers failed: %s!", buf );
 	}
 #else
