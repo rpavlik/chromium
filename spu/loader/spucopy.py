4b76b4c9eaa41f5dafd9eded27b439e8540fbc3b
Greg Humphreys <humper@users.sourceforge.net>
2001-03-10 04:00:42 +0000

    copy a dispatch table

---
+++ spu/loader/spucopy.py
@@ -0,0 +1,23 @@
+import sys,os;
+import cPickle;
+import string;
+import re;
+
+parsed_file = open( "../../glapi_parser/gl_header.parsed", "rb" )
+gl_mapping = cPickle.load( parsed_file )
+
+sys.path.append( "../../opengl_stub" )
+
+import stub_common;
+
+keys = gl_mapping.keys()
+keys.sort();
+print """#include "cr_spu.h"
+
+void crSPUCopyDispatchTable( SPUDispatchTable *dst, SPUDispatchTable *src )
+{"""
+
+for func_name in keys:
+	(return_type, names, types) = gl_mapping[func_name]
+	print '\tdst->%s = src->%s;' % (func_name,func_name)
+print '}'
