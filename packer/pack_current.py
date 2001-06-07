3fc92d7a30366fc32087a93ab2af98015986317a
Greg Humphreys <humper@users.sourceforge.net>
2001-06-07 01:43:40 +0000

    *** empty log message ***

---
+++ packer/pack_current.py
@@ -0,0 +1,43 @@
+from pack_currenttypes import *;
+
+print """
+#include "packer.h"
+#include "state/cr_currentpointers.h"
+
+void crPackOffsetCurrentPointers( int offset )
+{
+	GLnormal_p		*normal		= &(cr_packer_globals.current.normal);
+	GLcolor_p		*color		= &(cr_packer_globals.current.color);
+	GLtexcoord_p	*texcoord	= &(cr_packer_globals.current.texcoord);
+	GLindex_p		*index		= &(cr_packer_globals.current.index);
+	GLedgeflag_p	*edgeflag	= &(cr_packer_globals.current.edgeflag);
+
+"""
+
+for k in current_fns.keys():
+	name = k.lower()
+	for type in current_fns[k]['types']:
+		for size in current_fns[k]['sizes']:
+			ptr = "%s->%s%d" % (name, type, size )
+			print "\tif ( %s )" % ptr
+			print "\t\t%s += offset;" % ptr
+print """
+}
+
+void crPackNullCurrentPointers( void )
+{
+	GLnormal_p		*normal		= &(cr_packer_globals.current.normal);
+	GLcolor_p		*color		= &(cr_packer_globals.current.color);
+	GLtexcoord_p	*texcoord	= &(cr_packer_globals.current.texcoord);
+	GLindex_p		*index		= &(cr_packer_globals.current.index);
+	GLedgeflag_p	*edgeflag	= &(cr_packer_globals.current.edgeflag);
+"""
+
+for k in current_fns.keys():
+	name = k.lower()
+	for type in current_fns[k]['types']:
+		for size in current_fns[k]['sizes']:
+			ptr = "%s->%s%d" % (name, type, size )
+			print "\t%s = NULL;" % ptr
+
+print "}"
