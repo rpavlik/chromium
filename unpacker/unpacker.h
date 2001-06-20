10c2387553ee8d6755b4314d277c2a6fc70f6855
Greg Humphreys <humper@users.sourceforge.net>
2001-06-20 22:44:34 +0000

    export the unpacker pointer for current tracking

---
+++ unpacker/unpacker.h
@@ -0,0 +1,13 @@
+#ifndef CR_UNPACKER_H
+#define CR_UNPACKER_H
+
+#ifdef WINDOWS
+#ifdef DLLDATA
+#undef DLLDATA
+#endif
+#define DLLDATA __declspec(dllexport)
+#endif
+
+#include "cr_unpack.h"
+
+#endif /* CR_UNPACKER_H */
