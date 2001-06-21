e41f50a1aeaf2e41781e252817abda9ba3370407
Greg Humphreys <humper@users.sourceforge.net>
2001-06-21 21:15:01 +0000

    *** empty log message ***

---
+++ packer/packer_extensions.h
@@ -0,0 +1,8 @@
+#ifndef CR_PACKER_EXTENSIONS_H
+#define CR_PACKER_EXTENSIONS_H
+
+#include "cr_glwrapper.h"
+
+int crPackTexParameterParamsLength( GLenum pname );
+
+#endif /* CR_PACKER_EXTENSION_H */
