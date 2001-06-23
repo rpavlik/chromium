993f9c8f199452a543c4ac886171d2ed4d994ed9
Greg Humphreys <humper@users.sourceforge.net>
2001-06-23 19:44:37 +0000

    Beginnings of a PushAttrib/PopAttrib solution for the state_tracker.

---
+++ include/state/cr_attrib.h
@@ -0,0 +1,36 @@
+#ifndef CR_STATE_ATTRIB_H 
+#define CR_STATE_ATTRIB_H 
+
+#include "cr_glwrapper.h"
+#include "state/cr_statetypes.h"
+#include "state/cr_extensions.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define CR_MAX_ATTRIB_STACK_DEPTH 1024
+
+typedef struct {
+	GLbitvalue dirty;
+} CRAttribBits;
+
+typedef struct {
+	GLint attribStackDepth;
+	GLint maxAttribStackDepth;
+
+	GLbitvalue pushMaskStack[CR_MAX_ATTRIB_STACK_DEPTH];
+	GLint      pushMaskDepth;
+} CRAttribState;
+
+void crStateAttribInit(CRAttribState *a);
+
+// No diff!
+void crStateAttribSwitch(CRAttribBits *bb, GLbitvalue bitID, 
+		CRAttribState *from, CRAttribState *to);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CR_STATE_ATTRIB_H */
---
+++ spu/state_tracker/state_attrib.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <memory.h>
+#include "cr_glstate.h"
+#include "state/cr_statetypes.h"
+#include "state_internals.h"
+#include "state_extensionfuncs.h"
+#include "cr_error.h"
+
+void crStateAttribInit (CRAttribState *a) 
+{
+	a->attribStackDepth = 0;
+	a->maxAttribStackDepth = CR_MAX_ATTRIB_STACK_DEPTH; // XXX
+
+	a->pushMaskDepth = 0;
+}
+
+void STATE_APIENTRY crStatePushAttrib(GLbitfield mask)
+{
+	CRContext *g = GetCurrentContext();
+	CRAttribState *a = &(g->attrib);
+	CRStateBits *sb = GetCurrentBits();
+	CRAttribBits *ab = &(sb->attrib);
+
+	if (g->current.inBeginEnd)
+	{
+		crStateError(__LINE__, __FILE__, GL_INVALID_OPERATION, "glPushAttrib called in Begin/End");
+		return;
+	}
+
+	if (a->attribStackDepth == CR_MAX_ATTRIB_STACK_DEPTH - 1)
+	{
+		crStateError(__LINE__, __FILE__, GL_STACK_OVERFLOW, "glPushAttrib called with a full stack!" );
+	}
+	crDebug( "pushAttrib called with value 0x%x", mask );
+
+	a->pushMaskStack[a->pushMaskDepth++] = mask;
+
+	ab->dirty = g->neg_bitid;
+}
+
+void STATE_APIENTRY crStatePopAttrib(void) 
+{
+	CRContext *g = GetCurrentContext();
+	CRAttribState *a = &(g->attrib);
+	CRStateBits *sb = GetCurrentBits();
+	CRAttribBits *ab = &(sb->attrib);
+
+	GLbitvalue mask;
+
+	if (g->current.inBeginEnd)
+	{
+		crStateError(__LINE__, __FILE__, GL_INVALID_OPERATION, "glPopAttrib called in Begin/End");
+		return;
+	}
+
+	if (a->attribStackDepth == 0)
+	{
+		crStateError(__LINE__, __FILE__, GL_STACK_UNDERFLOW, "glPopAttrib called with an empty stack!" );
+	}
+
+	mask = a->pushMaskStack[a->pushMaskDepth--];
+
+	crDebug( "PopAttrib was called, the mask on the top of the stack was 0x%x", mask );
+
+	ab->dirty = g->neg_bitid;
+}
+
+void crStateAttribSwitch( CRAttribBits *bb, GLbitvalue bitID,
+		CRAttribState *from, CRAttribState *to )
+{
+	(void) bb;
+	(void) bitID;
+	(void) from;
+	(void) to;
+}
