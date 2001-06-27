713a182dd156190eda70136b42a0822f418bda77
Greg Humphreys <humper@users.sourceforge.net>
2001-06-27 05:38:06 +0000

    A couple of useful routines for printing opcode lists

---
+++ include/cr_debugopcodes.h
@@ -0,0 +1,9 @@
+#ifndef CR_DEBUG_OPCODES_H
+#define CR_DEBUG_OPCODES_H
+
+#include "cr_opcodes.h"
+#include <stdio.h>
+
+void crDebugOpcodes( FILE *fp, unsigned char *ptr, unsigned int num_opcodes );
+
+#endif /* CR_DEBUG_OPCODES_H */
---
+++ util/debug_opcodes.py
@@ -0,0 +1,40 @@
+import sys;
+import cPickle;
+import string;
+import re;
+
+sys.path.append( "../opengl_stub" )
+
+import stub_common;
+
+parsed_file = open( "../glapi_parser/gl_header.parsed", "rb" )
+gl_mapping = cPickle.load( parsed_file )
+
+print """
+#include "cr_debugopcodes.h"
+#include <stdio.h>
+"""
+
+print """void crDebugOpcodes( FILE *fp, unsigned char *ptr, unsigned int num_opcodes )
+{
+\tunsigned int i;
+\tfor (i = 0 ; i < num_opcodes ; i++)
+\t{
+\t\tswitch(*(ptr--))
+\t\t{
+"""
+
+keys = gl_mapping.keys()
+keys.sort();
+
+for func_name in keys:
+	if not stub_common.FindSpecial( "../packer/opcode", func_name ):
+		print '\t\tcase %s:' % stub_common.OpcodeName( func_name )
+		print '\t\t\tfprintf( fp, "%s\\n" ); ' % stub_common.OpcodeName( func_name )
+		print '\t\t\tbreak;'
+
+print """
+\t\t}
+\t}
+}
+"""
