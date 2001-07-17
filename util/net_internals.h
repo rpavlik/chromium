1c50b991e6bf17b715f6e252eeb8be49362f0b50
Greg Humphreys <humper@users.sourceforge.net>
2001-07-17 18:20:26 +0000

    Code to talk to the mothership independently of the client library (which would introduce a circular DLL dependency, yucky).

---
+++ util/net_internals.h
@@ -0,0 +1,8 @@
+#ifndef NET_INTERNALS_H
+#define NET_INTERNALS_H
+
+CRConnection *__copy_of_crMothershipConnect( void );
+int __copy_of_crMothershipReadResponse( CRConnection *conn, void *buf );
+int __copy_of_crMothershipSendString( CRConnection *conn, char *response_buf, char *str, ... );
+
+#endif /* NET_INTERNALS_H */
