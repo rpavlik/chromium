77f6405965426a9bec8aa18cd2de8625671f15fc
Greg Humphreys <humper@users.sourceforge.net>
2001-08-22 18:39:23 +0000

    this isn't auto generated either

---
+++ opengl_stub/api_templates.h
@@ -0,0 +1,13 @@
+/* Copyright (c) 2001, Stanford University
+	All rights reserved.
+
+	See the file LICENSE.txt for information on redistributing this software. */
+	
+#ifndef CR_API_TEMPLATES_H
+#define CR_API_TEMPLATES_H
+
+#include "cr_spu.h"
+
+extern SPUDispatchTable glim;
+
+#endif /* CR_API_TEMPLATES_H */
