d4e1e20a6d3eed5a1471991513faffda3cea7e06
Greg Humphreys <humper@users.sourceforge.net>
2001-08-22 22:24:59 +0000

    Reworked the sputest program slightly to demonstrate the API changing SPU.

--- spu/tester/sputest.c
+++ spu/tester/sputest.c
@@ -16,7 +16,9 @@ int main(int argc, char *argv[])
 	int ids[] = { 0, 1 };
 	char *spunames[] = { "apichangespu", "renderspu" };
 	float angle = 0;
-	int frame = 0;
+	GLfloat v1[3] = { .25, .25, 0 }; 
+	GLfloat v2[3] = { .25, .5, 0 }; 
+	GLfloat v3[3] = { .5, .25, 0 }; 
 
 	(void) argc;
 	(void) argv;
@@ -27,20 +29,15 @@ int main(int argc, char *argv[])
 	spu->dispatch_table.ClearColor( 0,0,0,1 );
 	for (;;)
 	{
-		frame++;
-		//if (frame == 3)
-		//{
-			//break;
-		//}
 		angle += ANGLE_STEP;
 		spu->dispatch_table.Clear( GL_COLOR_BUFFER_BIT );
 		spu->dispatch_table.MatrixMode( GL_MODELVIEW );
 		spu->dispatch_table.LoadIdentity();
 		spu->dispatch_table.Rotatef( angle, 0, 0, 1 );
 		spu->dispatch_table.Begin( GL_TRIANGLES );
-		spu->dispatch_table.Vertex3f( .5, .5, 0 );
-		spu->dispatch_table.Vertex3f( .5,1,0 );
-		spu->dispatch_table.Vertex3f( 1,.5,0 );
+		spu->dispatch_table.Vertex3fv( v1 );
+		spu->dispatch_table.Vertex3fv( v2 );
+		spu->dispatch_table.Vertex3fv( v3 );
 		spu->dispatch_table.End( );
 		spu->dispatch_table.SwapBuffers();
 	}
