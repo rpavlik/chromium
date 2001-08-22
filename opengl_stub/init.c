942f5b246281a14f7ab418b9be4742367bca0f8b
Greg Humphreys <humper@users.sourceforge.net>
2001-08-22 18:38:45 +0000

    This file is no longer auto-generated, so it's checked in now.

---
+++ opengl_stub/init.c
@@ -0,0 +1,19 @@
+/* Copyright (c) 2001, Stanford University
+	All rights reserved.
+
+	See the file LICENSE.txt for information on redistributing this software. */
+	
+#include "cr_spu.h"
+#include "api_templates.h"
+
+#ifdef WINDOWS
+// Let me cast function pointers to data pointers, I know what I'm doing.
+#pragma warning( disable: 4054 )
+#endif
+
+SPUDispatchTable glim;
+
+void FakerInit( SPU *spu )
+{
+	crSPUCopyDispatchTable( &glim, &(spu->dispatch_table) );
+}
