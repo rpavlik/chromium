8b58652bd7e64436dde871768fc2758c374ca3d4
Greg Humphreys <humper@users.sourceforge.net>
2001-08-22 18:42:29 +0000

    This file got out of sync with the dispatch table model.

--- opengl_stub/IRIX_edgeflagpointer.c
+++ opengl_stub/IRIX_edgeflagpointer.c
@@ -5,13 +5,11 @@
  */
 
 #include "cr_glwrapper.h"
+#include "api_templates.h"
 
 // This is necessary because IRIX disagrees with Windows about arg 2.
 
-extern void *__glim_EdgeFlagPointer;
-typedef void (*glEdgeFlagPointer_ptr) ( GLsizei stride, const GLboolean *pointer ) ;
-
 void glEdgeFlagPointer( GLsizei stride, const GLboolean *pointer )
 {
-	((glEdgeFlagPointer_ptr) __glim_EdgeFlagPointer)( stride, pointer );
+	glim.dgeFlagPointer( stride, pointer );
 }
