0cc688a93ddc68fa19366f9f79115384fa057a99
Peter D. Kirchner <pdkirchner@users.sourceforge.net>
2002-03-20 15:05:51 +0000

    file accommodates platform inconsistencies of API

---
+++ opengl_stub/AIX_edgeflagpointer.c
@@ -0,0 +1,15 @@
+/* Copyright (c) 2001, Stanford University
+ * All rights reserved
+ *
+ * See the file LICENSE.txt for information on redistributing this software.
+ */
+
+#include "cr_glwrapper.h"
+#include "api_templates.h"
+
+/* This is necessary because IRIX disagrees with Windows about arg 2. */
+
+void glEdgeFlagPointer( GLsizei stride, const void *pointer )
+{
+	glim.EdgeFlagPointer( stride, pointer );
+}
