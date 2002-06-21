ecae750d3ef3e8b9f1b042611f0be6a333a1c66d
Brian Paul <brianp@users.sourceforge.net>
2002-06-21 14:40:35 +0000

    move CreateWindow into packspu_context.c (not auto-generated)

--- spu/pack/packspu_context.c
+++ spu/pack/packspu_context.c
@@ -244,3 +244,34 @@ void PACKSPU_APIENTRY packspu_Finish( void )
 	while (writeback)
 		crNetRecv();
 }
+
+
+GLint PACKSPU_APIENTRY packspu_crCreateWindow( const char *dpyName, GLint visBits )
+{
+	GET_THREAD(thread);
+	int writeback = pack_spu.thread[0].server.conn->type == CR_DROP_PACKETS ? 0 : 1;
+	GLint return_val = (GLint) 0;
+	if (pack_spu.swap)
+	{
+		crPackcrCreateWindowSWAP( dpyName, visBits, &return_val, &writeback );
+	}
+	else
+	{
+		crPackcrCreateWindow( dpyName, visBits, &return_val, &writeback );
+	}
+	packspuFlush( (void *) thread );
+	if (pack_spu.thread[0].server.conn->type == CR_FILE) {
+		return 0;
+	}
+	else
+	{
+		while (writeback)
+			crNetRecv();
+		if (pack_spu.swap)
+		{
+			return_val = (GLint) SWAP32(return_val);
+		}
+		return return_val;
+	}
+}
+
