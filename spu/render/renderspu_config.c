82771191356f3c241c4e4729a872dafb5482f642
Peter D. Kirchner <pdkirchner@users.sourceforge.net>
2002-07-12 18:04:58 +0000

    man strncpy

--- spu/render/renderspu_config.c
+++ spu/render/renderspu_config.c
@@ -30,7 +30,7 @@ static void set_window_geometry( RenderSPU *render_spu, const char *response )
 
 static void set_display_string( RenderSPU *render_spu, const char *response )
 {
-   	strncpy(render_spu->display_string, response, strlen(render_spu->display_string));
+   	strncpy(render_spu->display_string, response, sizeof(render_spu->display_string));
 }
 
 static void set_fullscreen( RenderSPU *render_spu, const char *response )
