c9d190bf8fbf68c3939129c6492b6fa1b3ceeac5
Alan Hourihane <alanh@users.sourceforge.net>
2002-07-15 21:01:36 +0000

    Break out the architecture defines into arch.mk and include it in cr.mk
    This allows individual Makefiles to get the architecture specifics before
    including cr.mk.
    
    Fixes the Windows build.

---
+++ arch.mk
@@ -0,0 +1,35 @@
+# Copyright (c) 2001, Stanford University
+# All rights reserved.
+#
+# See the file LICENSE.txt for information on redistributing this software.
+
+ifneq ($(ARCH), WIN_NT)
+ifneq ($(ARCH), WIN_98)
+ARCH=$(shell uname | sed -e 's/-//g')
+MACHTYPE=$(shell uname -m)
+endif
+endif
+
+ifeq ($(ARCH), CYGWIN_NT5.0)
+ARCH=WIN_NT
+endif
+
+ifeq ($(ARCH), CYGWIN_NT5.1)
+ARCH=WIN_NT
+endif
+
+ifeq ($(MACHTYPE),i686)
+MACHTYPE=i386
+endif
+
+ifeq ($(MACHTYPE),i586)
+MACHTYPE=i386
+endif
+
+ifeq ($(MACHTYPE),i486)
+MACHTYPE=i386
+endif
+
+ECHO := echo
+
+include $(TOP)/config/$(ARCH).mk
--- cr.mk
+++ cr.mk
@@ -3,36 +3,7 @@
 #
 # See the file LICENSE.txt for information on redistributing this software.
 
-ifneq ($(ARCH), WIN_NT)
-ifneq ($(ARCH), WIN_98)
-ARCH=$(shell uname | sed -e 's/-//g')
-MACHTYPE=$(shell uname -m)
-endif
-endif
-
-ifeq ($(ARCH), CYGWIN_NT5.0)
-ARCH=WIN_NT
-endif
-
-ifeq ($(ARCH), CYGWIN_NT5.1)
-ARCH=WIN_NT
-endif
-
-ifeq ($(MACHTYPE),i686)
-MACHTYPE=i386
-endif
-
-ifeq ($(MACHTYPE),i586)
-MACHTYPE=i386
-endif
-
-ifeq ($(MACHTYPE),i486)
-MACHTYPE=i386
-endif
-
-ECHO := echo
-
-include $(TOP)/config/$(ARCH).mk
+include $(TOP)/arch.mk
 
 ###########################
 # LEAVE THESE THINGS ALONE!
