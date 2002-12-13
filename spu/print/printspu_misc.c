41a2227cd805e841cf188d491b92ccbbfd960456
Alan Hourihane <alanh@users.sourceforge.net>
2002-12-13 13:31:45 +0000

    fix ChromiumParameter(vi)CR to pass calls downstream Add Chromium's own enums (David Thompson)

--- spu/print/printspu_misc.c
+++ spu/print/printspu_misc.c
@@ -9,20 +9,68 @@
 
 void PRINT_APIENTRY printChromiumParametervCR(GLenum target, GLenum type, GLsizei count, const GLvoid *values)
 {
+	int i ;
 	switch (target) {
 	case GL_PRINT_STRING_CR:
 		fprintf( print_spu.fp, "%s\n", (char *)values );
 		fflush( print_spu.fp );
 		break;
+	default:
+		fprintf( print_spu.fp, "ChromiumParametervCR( " ) ;
+		fprintf( print_spu.fp, "%s, ", printspuEnumToStr( target ) ) ;
+		fprintf( print_spu.fp, "%s, ", printspuEnumToStr( type ) ) ;
+		fprintf( print_spu.fp, "%d, %p=[ ", count, values ) ;
+		switch( type ) {
+			case GL_INT:
+				for (i=0; i<count; i++)
+					fprintf( print_spu.fp, "%d ", ((int*)values)[i] ) ;
+				break ;
+			case GL_UNSIGNED_INT:
+				for (i=0; i<count; i++)
+					fprintf( print_spu.fp, "%u ", ((unsigned int*)values)[i] ) ;
+				break ;
+			case GL_SHORT:
+				for (i=0; i<count; i++)
+					fprintf( print_spu.fp, "%hd ", ((short*)values)[i] ) ;
+				break ;
+			case GL_UNSIGNED_SHORT:
+				for (i=0; i<count; i++)
+					fprintf( print_spu.fp, "%hu ", ((unsigned short*)values)[i] ) ;
+				break ;
+			case GL_BYTE:
+				for (i=0; i<count; i++)
+					fprintf( print_spu.fp, "%hhd ", ((char*)values)[i] ) ;
+				break ;
+			case GL_UNSIGNED_BYTE:
+				for (i=0; i<count; i++)
+					fprintf( print_spu.fp, "%hhu ", ((unsigned char*)values)[i] ) ;
+				break ;
+			case GL_FLOAT:
+				for (i=0; i<count; i++)
+					fprintf( print_spu.fp, "%g ", (double)((float*)values)[i] ) ;
+				break ;
+			case GL_DOUBLE:
+				for (i=0; i<count; i++)
+					fprintf( print_spu.fp, "%g ", ((double*)values)[i] ) ;
+				break ;
+			default:
+				fprintf( print_spu.fp, "unhandled fmt " ) ;
+				break ;
+		}
+		fprintf( print_spu.fp, "] )\n" ) ;
+		fflush( print_spu.fp ) ;
 	}
+	print_spu.passthrough.ChromiumParametervCR( target, type, count, values ) ;
 }
 
 void PRINT_APIENTRY printChromiumParameteriCR(GLenum target, GLint value)
 {
 	switch (target) {
 	default:
-		fprintf( print_spu.fp, "printspu ( %d, %d )\n", target, value );
+		fprintf( print_spu.fp, "ChromiumParameteri( %s, ", printspuEnumToStr(target) ) ;
+		fprintf( print_spu.fp, "%d )\n", value );
 		fflush( print_spu.fp );
 		break;
 	}
+	print_spu.passthrough.ChromiumParameteriCR( target, value ) ;
 }
