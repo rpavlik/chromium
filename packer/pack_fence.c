c105f4e71ba421fa9b0f1b7e5925fb77e3dd6279
Alan Hourihane <alanh@users.sourceforge.net>
2003-05-09 08:31:55 +0000

    fix usage of extended opcode

--- packer/pack_fence.c
+++ packer/pack_fence.c
@@ -14,9 +14,10 @@ crPackDeleteFencesNV(GLsizei n, const GLuint * fences)
 
 	data_ptr = (unsigned char *) crPackAlloc(packet_length);
 	WRITE_DATA(0, int, packet_length);
-	WRITE_DATA(sizeof(int) + 0, GLsizei, n);
-	crMemcpy(data_ptr + sizeof(int) + 4, fences, n * sizeof(*fences));
-	crHugePacket((CROpcode) CR_DELETEFENCESNV_EXTEND_OPCODE, data_ptr);
+	WRITE_DATA(4, GLenum, CR_DELETEFENCESNV_EXTEND_OPCODE);
+	WRITE_DATA(sizeof(int) + 4, GLsizei, n);
+	crMemcpy(data_ptr + sizeof(int) + 8, fences, n * sizeof(*fences));
+	crHugePacket(CR_EXTEND_OPCODE, data_ptr);
 }
 
 void PACK_APIENTRY crPackDeleteFencesNVSWAP( GLsizei n, const GLuint *fences )
@@ -27,12 +28,13 @@ void PACK_APIENTRY crPackDeleteFencesNVSWAP( GLsizei n, const GLuint *fences )
 
 	data_ptr = (unsigned char *) crPackAlloc( packet_length );
 	WRITE_DATA(0, int, packet_length);
-	WRITE_DATA(sizeof(int) + 0, GLsizei, n);
+	WRITE_DATA(4, GLenum, CR_DELETEFENCESNV_EXTEND_OPCODE);
+	WRITE_DATA(sizeof(int) + 4, GLsizei, n);
 	for (i = 0 ; i < n ; i++)
 	{
-		WRITE_DATA((i+1)*sizeof( int ) + 4, GLint, SWAP32(fences[i]));
+		WRITE_DATA((i+1)*sizeof( int ) + 8, GLint, SWAP32(fences[i]));
 	}
-	crHugePacket((CROpcode) CR_DELETEFENCESNV_EXTEND_OPCODE, data_ptr);
+	crHugePacket(CR_EXTEND_OPCODE, data_ptr);
 	crPackFree(data_ptr);
 }
 
