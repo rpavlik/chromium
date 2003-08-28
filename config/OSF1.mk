79ad37330725f078cfb1c94fc482baa9864f147c
Brian Paul <brianp@users.sourceforge.net>
2003-08-28 22:09:33 +0000

    use -g3 for release built (Joel Welling)

--- config/OSF1.mk
+++ config/OSF1.mk
@@ -5,11 +5,11 @@ CXX = cxx
 CC = cc
 
 CXXFLAGS          += -DOSF1 -Wall -Werror -pthread -DELAN_PRE_KITE
-CXX_RELEASE_FLAGS += -g -O3 -DNDEBUG
+CXX_RELEASE_FLAGS += -g3 -O3 -DNDEBUG
 CXX_DEBUG_FLAGS   += -g
 
 CFLAGS            += -DOSF1 -Wall -Werror -pthread -DELAN_PRE_KITE
-C_RELEASE_FLAGS   += -g -O3 -DNDEBUG
+C_RELEASE_FLAGS   += -g3 -O3 -DNDEBUG
 C_DEBUG_FLAGS     += -g
 
 LDFLAGS           += -L/usr/X11R6/lib
