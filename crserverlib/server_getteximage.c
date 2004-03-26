2427263ae4d5bdc37dd82fcb6cc6bd9413c54b3b
Brian Paul <brianp@users.sourceforge.net>
2004-03-26 01:45:28 +0000

    proper implementations of Get[Compressed]TexImage functions

--- crserverlib/server_getteximage.c
+++ crserverlib/server_getteximage.c
@@ -6,25 +6,61 @@
 
 #include "chromium.h"
 #include "cr_error.h" 
+#include "cr_mem.h"
+#include "cr_pixeldata.h"
 #include "server_dispatch.h"
 #include "server.h"
 
-void SERVER_DISPATCH_APIENTRY crServerDispatchGetTexImage( GLenum target, GLint level, GLenum format, GLenum type, GLvoid * pixels )
+void SERVER_DISPATCH_APIENTRY
+crServerDispatchGetTexImage(GLenum target, GLint level, GLenum format,
+														GLenum type, GLvoid * pixels)
 {
-	crError( "glGetTexImage isn't *ever* allowed to be on the wire!" );
-	(void) target;
-	(void) level;
-	(void) format;
-	(void) type;
-	(void) pixels;
+	GLsizei width, height, depth, size;
+	GLvoid *buffer;
+
+	cr_server.head_spu->dispatch_table.GetTexLevelParameteriv(target, level, GL_TEXTURE_WIDTH, &width);
+	cr_server.head_spu->dispatch_table.GetTexLevelParameteriv(target, level, GL_TEXTURE_HEIGHT, &height);
+	cr_server.head_spu->dispatch_table.GetTexLevelParameteriv(target, level, GL_TEXTURE_DEPTH, &depth);
+
+	size = crTextureSize(format, type, width, height, depth);
+
+	if (size && (buffer = crAlloc(size))) {
+		/* Note, the other pixel PACK parameters (default values) should
+		 * be OK at this point.
+		 */
+		cr_server.head_spu->dispatch_table.PixelStorei(GL_PACK_ALIGNMENT, 1);
+		cr_server.head_spu->dispatch_table.GetTexImage(target, level, format,
+																									 type, buffer);
+		crServerReturnValue( buffer, size );
+		crFree(buffer);
+	}
+	else {
+		crServerReturnValue( buffer, 0 );
+	}
 }
 
 
-void SERVER_DISPATCH_APIENTRY crServerDispatchGetCompressedTexImageARB( GLenum target, GLint level, GLvoid *img )
+#if CR_ARB_texture_compression
+
+void SERVER_DISPATCH_APIENTRY
+crServerDispatchGetCompressedTexImageARB(GLenum target, GLint level,
+																				 GLvoid *img)
 {
-	crError( "glGetCompressedTexImageARB isn't *ever* allowed to be on the wire!" );
-	(void) target;
-	(void) level;
-	(void) img;
+	GLint size;
+	GLvoid *buffer;
+
+	cr_server.head_spu->dispatch_table.GetTexLevelParameteriv(target, level, GL_TEXTURE_COMPRESSED_IMAGE_SIZE, &size);
+
+	if (size && (buffer = crAlloc(size))) {
+		/* XXX the pixel PACK parameter should be OK at this point */
+		cr_server.head_spu->dispatch_table.GetCompressedTexImageARB(target, level,
+																																buffer);
+		crServerReturnValue( buffer, size );
+		crFree(buffer);
+	}
+	else {
+		crServerReturnValue( buffer, 0 );
+	}
 }
 
+#endif /* CR_ARB_texture_compression */
