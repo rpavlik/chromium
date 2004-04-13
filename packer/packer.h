02926a366dd8b44ba28da09bc49a56b30771564b
Brian Paul <brianp@users.sourceforge.net>
2004-04-13 16:49:15 +0000

    remove redundant stuff that was already present in cr_pack.h

--- packer/packer.h
+++ packer/packer.h
@@ -18,18 +18,7 @@
 #include "cr_pack.h"
 #include "cr_packfunctions.h"
 #include "packer_extensions.h"
-#ifdef CHROMIUM_THREADSAFE
-#include "cr_threads.h"
-#endif
 #include "cr_mem.h"
-
-#ifdef CHROMIUM_THREADSAFE
-extern CRtsd _PackerTSD;
-#define GET_PACKER_CONTEXT(C) CRPackContext *C = (CRPackContext *) crGetTSD(&_PackerTSD)
-#else
-extern DLLDATA CRPackContext cr_packer_globals;
-#define GET_PACKER_CONTEXT(C) CRPackContext *C = &cr_packer_globals
-#endif
 
 extern void __PackError( int line, const char *file, GLenum error, const char *info );
 
