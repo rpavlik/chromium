03a53bf0d4fb684d87171ac2b3fbf0083cde5e4c
Brian Paul <brianp@users.sourceforge.net>
2004-06-25 22:39:25 +0000

    added ignore_freeglut_menus option

--- mothership/tools/crtypes.py
+++ mothership/tools/crtypes.py
@@ -774,6 +774,7 @@ class ApplicationNode(Node):
 			Option("track_window_size", "Track App Window Size Changes", "BOOL", 1, [0], [], []),
 			Option("track_window_Position", "Track App Window Position Changes", "BOOL", 1, [0], [], []),
 			Option("show_cursor", "Show Virtual Cursor", "BOOL", 1, [0], [], []),
+			Option("ignore_freeglut_menus", "Ignore freeglut's menus", "BOOL", 1, [1], [], []),
 			] )
 
 	def Clone(self):
