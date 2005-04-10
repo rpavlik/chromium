c0dba4b5534ff7d32e5ec352468124fe44e61774
Chris Waters <crwaters@users.sourceforge.net>
2005-04-10 10:10:35 +0000

    Fence pack/unpack fix/addition.

--- packer/pack_fence.c
+++ packer/pack_fence.c
@@ -10,29 +10,34 @@ void PACK_APIENTRY
 crPackDeleteFencesNV(GLsizei n, const GLuint * fences)
 {
 	unsigned char *data_ptr;
-	int packet_length = sizeof(int) + sizeof(n) + n * sizeof(*fences);
+	int packet_length = sizeof(GLenum) + sizeof(n) + n * sizeof(*fences);
+
+	if( !fences )
+		return;
 
 	data_ptr = (unsigned char *) crPackAlloc(packet_length);
-	WRITE_DATA(0, int, packet_length);
-	WRITE_DATA(4, GLenum, CR_DELETEFENCESNV_EXTEND_OPCODE);
-	WRITE_DATA(sizeof(int) + 4, GLsizei, n);
-	crMemcpy(data_ptr + sizeof(int) + 8, fences, n * sizeof(*fences));
+	WRITE_DATA(0, GLenum, CR_DELETEFENCESNV_EXTEND_OPCODE);
+	WRITE_DATA(4, GLsizei, n);
+	crMemcpy(data_ptr + 8, fences, n * sizeof(*fences));
 	crHugePacket(CR_EXTEND_OPCODE, data_ptr);
+	crPackFree(data_ptr);
 }
 
 void PACK_APIENTRY crPackDeleteFencesNVSWAP( GLsizei n, const GLuint *fences )
 {
 	unsigned char *data_ptr;
 	int i;
-	int packet_length = sizeof(int) + sizeof(n) + n * sizeof(*fences);
+	int packet_length = sizeof(GLenum) + sizeof(n) + n * sizeof(*fences);
+
+	if( !fences )
+		return;
 
 	data_ptr = (unsigned char *) crPackAlloc( packet_length );
-	WRITE_DATA(0, int, packet_length);
-	WRITE_DATA(4, GLenum, CR_DELETEFENCESNV_EXTEND_OPCODE);
-	WRITE_DATA(sizeof(int) + 4, GLsizei, n);
+	WRITE_DATA(0, GLenum, CR_DELETEFENCESNV_EXTEND_OPCODE);
+	WRITE_DATA(4, GLsizei, n);
 	for (i = 0 ; i < n ; i++)
 	{
-		WRITE_DATA((i+1)*sizeof( int ) + 8, GLint, SWAP32(fences[i]));
+		WRITE_DATA(i*sizeof(GLuint) + 8, GLuint, SWAP32(fences[i]));
 	}
 	crHugePacket(CR_EXTEND_OPCODE, data_ptr);
 	crPackFree(data_ptr);
