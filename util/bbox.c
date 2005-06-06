69aafaa771c76394c969467886093542f3ee8db8
Mike Houston <mikehouston@users.sourceforge.net>
2005-06-06 17:48:46 +0000

    Clean up ProjectBBox to use TransformBBox.  This also magically fixes the compositing errors seen with raptor occasionally.

--- util/bbox.c
+++ util/bbox.c
@@ -24,14 +24,14 @@ static float _vmult(const float *m, float x, float y, float z)
 
 void
 crTransformBBox( float xmin, float ymin, float zmin,
-								 float xmax, float ymax, float zmax,
-								 const CRmatrix *m,
-								 float *out_xmin, float *out_ymin, float *out_zmin,
-								 float *out_xmax, float *out_ymax, float *out_zmax )
+                 float xmax, float ymax, float zmax,
+                 const CRmatrix *m,
+                 float *out_xmin, float *out_ymin, float *out_zmin,
+                 float *out_xmax, float *out_ymax, float *out_zmax )
 {
 	float x[8], y[8], z[8], w[8];
 	int i,j;
-
+    
 	/*  Here is the arrangement of the bounding box
 	 *  
 	 *           0 --- 1
@@ -172,18 +172,6 @@ crTransformBBox( float xmin, float ymin, float zmin,
 	if (out_zmax) *out_zmax = zmax;
 }
 
-
-
-#define MTRANSFORM(x, y, z, w, m, vx, vy, vz) \
-    x = m[0]*vx + m[4]*vy + m[8]*vz  + m[12]; \
-    y = m[1]*vx + m[5]*vy + m[9]*vz  + m[13]; \
-    z = m[2]*vx + m[6]*vy + m[10]*vz + m[14]; \
-    w = m[3]*vx + m[7]*vy + m[11]*vz + m[15]
-
-#define I_TRANSFORM(num, m, vx, vy, vz) \
-    MTRANSFORM (x[num], y[num], z[num], w[num], m, vx, vy, vz)
-
-
 /**
  * Given the coordinates of the two opposite corners of a bounding box
  * in object space (x1,y1,z1) and (x2,y2,z2), use the given modelview
@@ -193,73 +181,34 @@ crTransformBBox( float xmin, float ymin, float zmin,
  */
 void
 crProjectBBox(const GLfloat modl[16], const GLfloat proj[16], 
-							GLfloat *x1, GLfloat *y1, GLfloat *z1,
-							GLfloat *x2, GLfloat *y2, GLfloat *z2) 
+              GLfloat *x1, GLfloat *y1, GLfloat *z1,
+              GLfloat *x2, GLfloat *y2, GLfloat *z2) 
 {
-	static GLfloat m[16];
-	int i;
-	
-	GLfloat x[8], y[8], z[8], w[8];
-	GLfloat vx1, vy1, vz1;
-	GLfloat vx2, vy2, vz2;
-	
-	GLfloat xmin = FLT_MAX, ymin = FLT_MAX, zmin = FLT_MAX;
-	GLfloat xmax = -FLT_MAX, ymax = -FLT_MAX, zmax = -FLT_MAX;
+	CRmatrix m;
 	
 	/* compute product of modl and proj matrices */
-	m[0]  = proj[0] * modl[0]  + proj[4] * modl[1]  + proj[8]  * modl[2]  + proj[12] * modl[3];	
-	m[1]  = proj[1] * modl[0]  + proj[5] * modl[1]  + proj[9]  * modl[2]  + proj[13] * modl[3];	
-	m[2]  = proj[2] * modl[0]  + proj[6] * modl[1]  + proj[10] * modl[2]  + proj[14] * modl[3];	
-	m[3]  = proj[3] * modl[0]  + proj[7] * modl[1]  + proj[11] * modl[2]  + proj[15] * modl[3];	
-	m[4]  = proj[0] * modl[4]  + proj[4] * modl[5]  + proj[8]  * modl[6]  + proj[12] * modl[7];	
-	m[5]  = proj[1] * modl[4]  + proj[5] * modl[5]  + proj[9]  * modl[6]  + proj[13] * modl[7];	
-	m[6]  = proj[2] * modl[4]  + proj[6] * modl[5]  + proj[10] * modl[6]  + proj[14] * modl[7];	
-	m[7]  = proj[3] * modl[4]  + proj[7] * modl[5]  + proj[11] * modl[6]  + proj[15] * modl[7];	
-	m[8]  = proj[0] * modl[8]  + proj[4] * modl[9]  + proj[8]  * modl[10] + proj[12] * modl[11];	
-	m[9]  = proj[1] * modl[8]  + proj[5] * modl[9]  + proj[9]  * modl[10] + proj[13] * modl[11];
-	m[10] = proj[2] * modl[8]  + proj[6] * modl[9]  + proj[10] * modl[10] + proj[14] * modl[11];	
-	m[11] = proj[3] * modl[8]  + proj[7] * modl[9]  + proj[11] * modl[10] + proj[15] * modl[11];	
-	m[12] = proj[0] * modl[12] + proj[4] * modl[13] + proj[8]  * modl[14] + proj[12] * modl[15];	
-	m[13] = proj[1] * modl[12] + proj[5] * modl[13] + proj[9]  * modl[14] + proj[13] * modl[15];	
-	m[14] = proj[2] * modl[12] + proj[6] * modl[13] + proj[10] * modl[14] + proj[14] * modl[15];	
-	m[15] = proj[3] * modl[12] + proj[7] * modl[13] + proj[11] * modl[14] + proj[15] * modl[15]; 
+	m.m00  = proj[0] * modl[0]  + proj[4] * modl[1]  + proj[8]  * modl[2]  + proj[12] * modl[3];	
+	m.m01  = proj[1] * modl[0]  + proj[5] * modl[1]  + proj[9]  * modl[2]  + proj[13] * modl[3];	
+	m.m02  = proj[2] * modl[0]  + proj[6] * modl[1]  + proj[10] * modl[2]  + proj[14] * modl[3];	
+	m.m03  = proj[3] * modl[0]  + proj[7] * modl[1]  + proj[11] * modl[2]  + proj[15] * modl[3];	
+	m.m10  = proj[0] * modl[4]  + proj[4] * modl[5]  + proj[8]  * modl[6]  + proj[12] * modl[7];	
+	m.m11  = proj[1] * modl[4]  + proj[5] * modl[5]  + proj[9]  * modl[6]  + proj[13] * modl[7];	
+	m.m12  = proj[2] * modl[4]  + proj[6] * modl[5]  + proj[10] * modl[6]  + proj[14] * modl[7];	
+	m.m13  = proj[3] * modl[4]  + proj[7] * modl[5]  + proj[11] * modl[6]  + proj[15] * modl[7];	
+	m.m20  = proj[0] * modl[8]  + proj[4] * modl[9]  + proj[8]  * modl[10] + proj[12] * modl[11];	
+	m.m21  = proj[1] * modl[8]  + proj[5] * modl[9]  + proj[9]  * modl[10] + proj[13] * modl[11];
+	m.m22  = proj[2] * modl[8]  + proj[6] * modl[9]  + proj[10] * modl[10] + proj[14] * modl[11];	
+	m.m23  = proj[3] * modl[8]  + proj[7] * modl[9]  + proj[11] * modl[10] + proj[15] * modl[11];	
+	m.m30  = proj[0] * modl[12] + proj[4] * modl[13] + proj[8]  * modl[14] + proj[12] * modl[15];	
+	m.m31  = proj[1] * modl[12] + proj[5] * modl[13] + proj[9]  * modl[14] + proj[13] * modl[15];	
+	m.m32  = proj[2] * modl[12] + proj[6] * modl[13] + proj[10] * modl[14] + proj[14] * modl[15];	
+	m.m33  = proj[3] * modl[12] + proj[7] * modl[13] + proj[11] * modl[14] + proj[15] * modl[15]; 
 	
-	/* Tranform the point by m */
-	vx1 = *x1;
-	vy1 = *y1;
-	vz1 = *z1;
-	vx2 = *x2;
-	vy2 = *y2;
-	vz2 = *z2;
-	
-	I_TRANSFORM(0 , m, vx1, vy1, vz1);
-	I_TRANSFORM(1 , m, vx1, vy1, vz2);
-	I_TRANSFORM(2 , m, vx1, vy2, vz1);
-	I_TRANSFORM(3 , m, vx1, vy2, vz2);
-	I_TRANSFORM(4 , m, vx2, vy1, vz1);
-	I_TRANSFORM(5 , m, vx2, vy1, vz2);
-	I_TRANSFORM(6 , m, vx2, vy2, vz1);
-	I_TRANSFORM(7 , m, vx2, vy2, vz2);
-	
-	for (i = 0; i < 8; i++) {   
-		x[i] /= w[i];
-		y[i] /= w[i];
-		z[i] /= w[i];
-		
-		if (x[i] > xmax) xmax = x[i];
-		if (y[i] > ymax) ymax = y[i];
-		if (z[i] > zmax) zmax = z[i]; 
-		if (x[i] < xmin) xmin = x[i];
-		if (y[i] < ymin) ymin = y[i];
-		if (z[i] < zmin) zmin = z[i];
-	}    
-	
-	*x1 = xmin;
-	*y1 = ymin;
-	*z1 = zmin;
-	*x2 = xmax;
-	*y2 = ymax;
-	*z2 = zmax;
+    crTransformBBox( *x1, *y1, *z1,
+                     *x2, *y2, *z2,
+                     &m,
+                     x1, y1, z1,
+                     x2, y2, z2 );
 }
 
 
