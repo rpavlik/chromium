04f4e4d63fb8998a0a55481fc92c58a237a68567
Brian Paul <brianp@users.sourceforge.net>
2006-05-12 21:45:48 +0000

    Updated to use new cr_pack.h functions.

--- packer/pack_buffer.c
+++ packer/pack_buffer.c
@@ -11,7 +11,6 @@
 #include "cr_protocol.h"
 #include "cr_unpack.h"
 
-#include <stdio.h>
 
 void crWriteUnalignedDouble( void *buffer, double d )
 {
@@ -154,18 +153,35 @@ void crPackResetPointers( CRPackContext *pc )
 	pc->buffer.canBarf = canBarf;
 }
 
-/* Each opcode has at least a 1-word payload, so opcodes can occupy at most 
- * 20% of the space. */
-int crPackNumOpcodes( int buffer_size ) {
-	/* Don't forget to add one here
-	 * in case the buffer size is not divisible by 4
-	 * thanks to Ken Moreland for finding this */
-	return ((( buffer_size - sizeof(CRMessageOpcodes) ) / 5) + 1 + 0x3)&(~0x3);
-												/* round up to multiple of 4 */
+
+/**
+ * Return max number of opcodes that'll fit in the given buffer size.
+ * Each opcode has at least a 1-word payload, so opcodes can occupy at most 
+ * 20% of the space.
+ */
+int
+crPackMaxOpcodes( int buffer_size )
+{
+	int n = ( buffer_size - sizeof(CRMessageOpcodes) ) / 5;
+	/* Don't forget to add one here in case the buffer size is not
+	 * divisible by 4.  Thanks to Ken Moreland for finding this.
+	 */
+	n++;
+	/* round up to multiple of 4 */
+	n = (n + 0x3) & (~0x3);
+	return n;
 }
-int crPackNumData( int buffer_size ) {
-	return buffer_size - sizeof(CRMessageOpcodes)
-		- crPackNumOpcodes(buffer_size);
+
+
+/**
+ * Return max number of data bytes that'll fit in the given buffer size.
+ */
+int
+crPackMaxData( int buffer_size )
+{
+	int n = buffer_size - sizeof(CRMessageOpcodes);
+	n -= crPackMaxOpcodes(buffer_size);
+	return n;
 }
 
 
@@ -212,7 +228,7 @@ void crPackInitBuffer( CRPackBuffer *buf, void *pack, int size, int mtu )
 	buf->mtu  = mtu;
 	buf->pack = pack;
 
-	num_opcodes = crPackNumOpcodes( buf->size );
+	num_opcodes = crPackMaxOpcodes( buf->size );
 
 	buf->data_start    = 
 		(unsigned char *) buf->pack + num_opcodes + sizeof(CRMessageOpcodes);
@@ -237,45 +253,29 @@ void crPackInitBuffer( CRPackBuffer *buf, void *pack, int size, int mtu )
 	}
 }
 
-#if 0
-int crPackCanHoldOpcode( int num_opcode, int num_data )
-{
-	int fitsInMTU, opcodesFit, dataFits;
-	GET_PACKER_CONTEXT(pc);
-
-	CRASSERT(pc->currentBuffer);
-
-	fitsInMTU = (((pc->buffer.data_current - pc->buffer.opcode_current - 1
-								 + num_opcode + num_data
-								 + 0x3 ) & ~0x3) + sizeof(CRMessageOpcodes)
-							 <= pc->buffer.mtu);
-	opcodesFit = (pc->buffer.opcode_current - num_opcode
-								>= pc->buffer.opcode_end);
-	dataFits = (pc->buffer.data_current + num_data <= pc->buffer.data_end);
-
-	return fitsInMTU && opcodesFit && dataFits;
-}
-#endif
 
 int crPackCanHoldBuffer( const CRPackBuffer *src )
 {
-	const int num_data = src->data_current - src->data_start;
-	const int num_opcode = src->opcode_start - src->opcode_current;
-	return crPackCanHoldOpcode( num_opcode, num_data );
+	const int num_data = crPackNumData(src);
+	const int num_opcode = crPackNumOpcodes(src);
+	GET_PACKER_CONTEXT(pc);
+	return crPackCanHoldOpcode( pc, num_opcode, num_data );
 }
+
 
 int crPackCanHoldBoundedBuffer( const CRPackBuffer *src )
 {
 	const int len_aligned = (src->data_current - src->opcode_current - 1 + 3) & ~3;
+	GET_PACKER_CONTEXT(pc);
 	/* 24 is the size of the bounds-info packet... */
-	return crPackCanHoldOpcode( 1, len_aligned + 24 );
+	return crPackCanHoldOpcode( pc, 1, len_aligned + 24 );
 }
 
 void crPackAppendBuffer( const CRPackBuffer *src )
 {
 	GET_PACKER_CONTEXT(pc);
-	const int num_data = src->data_current - src->data_start; /* in bytes */
-	const int num_opcode = src->opcode_start - src->opcode_current; /* bytes */
+	const int num_data = crPackNumData(src);
+	const int num_opcode = crPackNumOpcodes(src);
 
 	CRASSERT(num_data >= 0);
 	CRASSERT(num_opcode >= 0);
@@ -310,11 +310,12 @@ void crPackAppendBuffer( const CRPackBuffer *src )
 }
 
 
-void crPackAppendBoundedBuffer( const CRPackBuffer *src, const CRrecti *bounds )
+void
+crPackAppendBoundedBuffer( const CRPackBuffer *src, const CRrecti *bounds )
 {
 	GET_PACKER_CONTEXT(pc);
 	const GLbyte *payload = (const GLbyte *) src->opcode_current + 1;
-	const int num_opcodes = src->opcode_start - src->opcode_current;
+	const int num_opcodes = crPackNumOpcodes(src);
 	const int length = src->data_current - src->opcode_current - 1;
 
 	CRASSERT(pc);
@@ -365,7 +366,7 @@ void *crPackAlloc( unsigned int size )
 	/* include space for the length and make the payload word-aligned */
 	size = ( size + sizeof(unsigned int) + 0x3 ) & ~0x3;
 
-	if ( crPackCanHoldOpcode( 1, size ) )
+	if ( crPackCanHoldOpcode( pc, 1, size ) )
 	{
 		/* we can just put it in the current buffer */
 		GET_BUFFERED_POINTER(pc, size );  /* NOTE: this sets data_ptr */
@@ -374,7 +375,7 @@ void *crPackAlloc( unsigned int size )
 	{
 		/* Okay, it didn't fit.  Maybe it will after we flush. */
 		pc->Flush( pc->flush_arg );
-		if ( crPackCanHoldOpcode( 1, size ) )
+		if ( crPackCanHoldOpcode( pc, 1, size ) )
 		{
 			GET_BUFFERED_POINTER(pc, size );  /* NOTE: this sets data_ptr */
 		}
