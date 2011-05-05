cc944e40c0fbbff60d7545a602edebecc83dd03f
Ryan Pavlik <rpavlik@iastate.edu>
2011-05-05 12:04:49 -0500

    make it recursive

--- grabcmake.py
+++ grabcmake.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
-pathToGrabber = "../grab.mk"
+pathToGrabber = os.path.abspath("grab.mk")
 def getVariable(var):
 	proc = subprocess.Popen(["make", "--silent", "-f", pathToGrabber, "GETVAR", "VARNAME=%s" % var], stdout = subprocess.PIPE)
 	return proc.communicate()[0].strip()
@@ -26,18 +26,29 @@ def getTarget():
 		else:
 			return lib, "add_library(%s STATIC ${SOURCES})\n" % lib
 	else:
-		raise Exception("Not a lib or a program!")
+		return None, ""
+
+def doDirectory(sourcedir):
+	print sourcedir
+	os.chdir(sourcedir)
+	with open("CMakeLists.txt","w") as cmake:
+		target, targetline = getTarget()
+		if target is not None:
+			cmake.write("set(SOURCES\n")
+			cmake.writelines(["\t%s\n" % fn for fn in getSources()])
+			cmake.write(")\n")
+
+			cmake.write(targetline)
+
+			libs = [ lib.replace("-l", "") for lib in getVariableList("LIBRARIES") ]
+			if len(libs) > 0:
+				cmake.write("target_link_libraries(%s %s)\n" % (target, " ".join(libs)))
+
+		dirs = getVariableList("SUBDIRS")
+		if len(dirs) > 0:
+			cmake.writelines(["add_subdirectory(%s)\n" % dirname for dirname in dirs])
 
 
-with open("CMakeLists.txt","w") as cmake:
-	cmake.write("set(SOURCES\n")
-	cmake.writelines(["\t%s\n" % fn for fn in getSources()])
-	cmake.write(")\n")
-
-	target, targetline = getTarget()
-	cmake.write(targetline)
-
-	libs = getVariableList("LIBRARIES")
-	if len(libs) > 0:
-		cmake.write("target_link_libraries(%s %s)" % (target, " ".join(libs)))
-
+sourcedirs = [ dirpath for (dirpath, dirnames, filenames) in os.walk(os.path.abspath(".")) if "Makefile" in filenames]
+for sourcedir in sourcedirs:
+	doDirectory(sourcedir)
