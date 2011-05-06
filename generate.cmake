bdb444a58c4def3968e296ad318af4c8c80a5e4a
Ryan Pavlik <rpavlik@iastate.edu>
2011-05-06 16:55:49 -0500

    let progs generate

--- generate.cmake
+++ generate.cmake
@@ -31,3 +31,23 @@ function(generate_with_python_arg script arg output)
 		${CMAKE_CURRENT_SOURCE_DIR}/${script}
 		${ARGN})
 endfunction()
+
+function(create_local_apifiles)
+	file(RELATIVE_PATH TOP ${CMAKE_CURRENT_SOURCE_DIR} ${PROJECT_SOURCE_DIR})
+	set(LOCAL_APIDIR ${CMAKE_CURRENT_BINARY_DIR}/${TOP}/glapi_parser)
+	file(MAKE_DIRECTORY ${LOCAL_APIDIR})
+
+	add_custom_command(OUTPUT
+		${PROJECT_SOURCE_DIR}/glapi_parser/APIspec.txt
+		COMMAND
+		${CMAKE_COMMAND} -E copy_if_different ${PROJECT_SOURCE_DIR}/glapi_parser/APIspec.txt ${LOCAL_APIDIR}/APIspec.txt
+		DEPENDS
+		${PROJECT_SOURCE_DIR}/glapi_parser/APIspec.txt)
+	add_custom_command(OUTPUT
+		${PROJECT_SOURCE_DIR}/glapi_parser/apiutil.py
+		COMMAND
+		${CMAKE_COMMAND} -E copy_if_different ${PROJECT_SOURCE_DIR}/glapi_parser/apiutil.py ${LOCAL_APIDIR}/apiutil.py
+		DEPENDS
+		${PROJECT_SOURCE_DIR}/glapi_parser/apiutil.py)
+	set(LOCAL_APIFILES "${LOCAL_APIDIR}/APIspec.txt" "${LOCAL_APIDIR}/apiutil.py" PARENT_SCOPE)
+endfunction()
